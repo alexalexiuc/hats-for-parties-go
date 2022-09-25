000a333f3e209017c2390eaae407836412cf8d76
Alexiuc Alex <alexalexiuc@safeguardglobal.com>
2022-09-25 15:50:24 +0300

    Final touches

--- app/utils/utils.go
+++ app/utils/utils.go
@@ -1,10 +1,14 @@
 package utils
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func RandString(n int) string {
+	rand.Seed(time.Now().Unix())
 	b := make([]byte, n)
 	for i := range b {
 		b[i] = letters[rand.Intn(len(letters))]
